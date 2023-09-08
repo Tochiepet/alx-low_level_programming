ab4a2db295ce52615e9a3e19c2bafc2e6d03dca0
Tochiepet <alepeter215@gmail.com>
2023-09-07 17:11:31 -0700

    Hello world project

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this prints this program as run by main
+ *
+ * Return: return 0 all the time
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
