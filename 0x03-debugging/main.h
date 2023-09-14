c129ccc2af330a644cc6a61568c069ae8a7c6fe1
Tochiepet <alepeter215@gmail.com>
2023-09-14 10:29:44 -0700

    Debugging task 1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,5 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_num(int a, int b, int c);
-int convert_day(int montn, int day);
-void print_remaianing_days(int month, int day, int year);
 
 #endif /* MAIN_H */
